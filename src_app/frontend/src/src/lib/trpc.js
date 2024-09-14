aea6c43495c9653b6b6021e010c4365185227e2f
deh-code <rbnflt3@gmail.com>
2024-09-14 11:52:53 +0200

    Aggiunge type support con jsdoc per trpc

--- src_app/frontend/src/src/lib/trpc.js
+++ src_app/frontend/src/src/lib/trpc.js
@@ -1,5 +1,10 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 
+/**
+ * @typedef {import('../../../../backend/src/index.ts').AppRouter} AppRouter
+ */
+
+/** @type {import('@trpc/client').CreateTRPCProxyClient<AppRouter>} */
 export const trpc = createTRPCProxyClient({
   links: [
     httpBatchLink({
