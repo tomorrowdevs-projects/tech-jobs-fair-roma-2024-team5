8defd2df4064ecbbe3f9efb6b2ccd9d4ee504d6b
r.fileti <rbnflt3@gmail.com>
2024-09-20 07:42:47 +0200

    Aggiorna frequenza esecuzinoe cron

--- src_app/backend/src/index.ts
+++ src_app/backend/src/index.ts
@@ -50,7 +50,7 @@ app.listen(3001, () => {
   console.log('Server running on http://localhost:3001');
 });
 
-cron.schedule('* * * * *', () => {
+cron.schedule('0 8 * * *', () => {
   sendNotification()
 });
 
